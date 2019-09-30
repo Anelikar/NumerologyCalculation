571290cf09df60276b2904bf7db22296ba2b980f
Anelikar <TBC2.0@yandex.ru>
2019-09-30 14:23:28 +0700

    Changed encryption algorithm used in code to actually support 'auto' option in secutity.yaml

--- src/DataFixtures/UserFixture.php
+++ src/DataFixtures/UserFixture.php
@@ -7,9 +7,16 @@ use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Component\Security\Core\Encoder\Argon2iPasswordEncoder;
 use Symfony\Component\Security\Core\Encoder\SodiumPasswordEncoder;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class UserFixture extends Fixture
 {
+    private $encoder;
+
+    public function __construct(UserPasswordEncoderInterface $passwordEncoder){
+        $this->encoder = $passwordEncoder;
+    }
+
     public function load(ObjectManager $manager)
     {
         $user = new User();
@@ -17,8 +24,7 @@ class UserFixture extends Fixture
         $user->setApiToken("{DD010F67-7E23-473E-B867-019CEDDF016F}");
 
         $user->setSalt("{FD5C7D8A-DCE5-48F7-9D11-C3ABC77E5A43}");
-        $encoder = new SodiumPasswordEncoder();
-        $user->setPassword($encoder->encodePassword("{FF24892C-D4E3-4042-81E7-3F81C6A5A82C}", $user->getSalt()));
+        $user->setPassword($this->encoder->encodePassword($user,"{FF24892C-D4E3-4042-81E7-3F81C6A5A82C}"));
 
         $manager->persist($user);
 
--- src/Security/TokenAuthenticator.php
+++ src/Security/TokenAuthenticator.php
@@ -16,6 +16,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Encoder\Argon2iPasswordEncoder;
 use Symfony\Component\Security\Core\Encoder\SodiumPasswordEncoder;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use Symfony\Component\Security\Core\Exception\AuthenticationException;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Security\Core\User\UserProviderInterface;
@@ -24,10 +25,13 @@ use Symfony\Component\Security\Guard\AbstractGuardAuthenticator;
 class TokenAuthenticator extends AbstractGuardAuthenticator
 {
     private $em;
+    private $encoder;
 
-    public function __construct(EntityManagerInterface $em)
+    public function __construct(EntityManagerInterface $em,
+        UserPasswordEncoderInterface $passwordEncoder)
     {
         $this->em = $em;
+        $this->encoder = $passwordEncoder;
     }
 
     /**
@@ -67,8 +71,7 @@ class TokenAuthenticator extends AbstractGuardAuthenticator
 
     public function checkCredentials($credentials, UserInterface $user)
     {
-        $encoder = new SodiumPasswordEncoder();
-        if ($encoder->isPasswordValid($user->getPassword(), $credentials['password'], $user->getSalt())){
+        if ($this->encoder->isPasswordValid($user, $credentials['password'])){
             return true;
         }
 
